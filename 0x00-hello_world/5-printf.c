2adfe93400bc9db437964a1261bc76effd9c244b
Dennis Meme <dennismeme46@gmail.com>
2023-03-16 11:46:13 -0700

    mwasarai trial

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program that prints exactly
+ * Return: 0 always success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,'\n'");
+		return (0);
+}
