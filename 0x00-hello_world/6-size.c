d116322ce5b4855620970f3437b6ab40531b477a
Dennis Meme <dennismeme46@gmail.com>
2023-03-16 18:19:35 -0700

    betty3

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,13 +2,13 @@
 /**
  * main - A program that prints the sizes of data types
  * Return 0 (success)
- * /
+ */
 int main(void)
 {
-printf("Size of a char: %lu byte (s)\n", (unsigned long) sizeof (char));
-printf("Size of an int: %lu byte (s)\n", (unsigned long) sizeof (int));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof (long int));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof (long long int));
-printf("Size of float: %lu byte(s)\n", (unsigned long) sizeof (float));
+printf("Size of a char: %lu byte (s)\n", (unsigned long) sizeof(char));
+printf("Size of an int: %lu byte (s)\n", (unsigned long) sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(long long int));
+printf("Size of float: %lu byte(s)\n", (unsigned long) sizeof(float));
 return (0);
 }
