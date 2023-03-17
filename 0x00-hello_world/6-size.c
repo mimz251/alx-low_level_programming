fa5cae4e1960f1ec1ca90393ca6a34bdba2f95e2
Dennis Meme <dennismeme46@gmail.com>
2023-03-16 17:54:22 -0700

    Betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,10 @@
  * /
 int main(void)
 {
-            char a;
-            int b;
-            long int c;
-            long long int d;
-            float f;
-printf("Size of a char: % lu byte (s)/n", (unsigned long)size of (a));
-printf("Size of an int: % lu byte (s)/n", (unsigned long)size of (b));
-printf("size of a long int: % lu byte (s)/n", (unsigned long)size of (c));
-printf("size of a long long int: % lu byte (s)/n", (unsigned long)size of (d));
-printf("size of float: % lu byte (s)/n", (unsigned long)size of (f));
+printf("Size of a char: %lu byte (s)", (unsigned long) sizeof (char));
+printf("Size of an int: %lu byte (s)", (unsigned long) sizeof (int));
+printf("Size of a long int: %lu byte (s)", (unsigned long) sizeof (long int));
+printf("Size of a long long int: %lu byte (s)", (unsigned long) sizeof (long long int));
+printf("Size of float: %lu byte (s)", (unsigned long) sizeof (float));
 return (0);
 }
