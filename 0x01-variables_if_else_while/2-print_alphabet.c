d901f8d944ef75fffd5b35eb9392cc666b5ebea0
Dennis Meme <dennismeme46@gmail.com>
2023-03-18 07:42:40 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabet in lowercase.
+ * Return: always (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+return (0);
+}
