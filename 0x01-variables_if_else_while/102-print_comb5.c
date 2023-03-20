3cc6c81392c7b801e20b16f7c14716ab38b3e15b
Dennis Meme <dennismeme46@gmail.com>
2023-03-20 07:57:43 -0700

    bbhjf

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,10 +11,10 @@ int main(void)
 {
 	int p, q;
 
-	for (p = 0; p <= 98; p++)
+	for (p = 0; p <= 99; p++)
 	{
 
-	for (q = p + 1; q <= 98; q++)
+	for (q = p + 1; q <= 99; q++)
 	{
 	putchar((p / 10) + '0');
 	putchar((p % 10) + '0');
