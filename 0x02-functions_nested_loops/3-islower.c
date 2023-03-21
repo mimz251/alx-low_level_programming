a6073c7949d0cd6fb9e1a502ad4c8e6b560e5cad
Dennis Meme <dennismeme46@gmail.com>
2023-03-21 08:57:45 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,19 @@
 #include "main.h"
 /**
  *_islower - a function that checks for lowercase character.
+ * @c: it is the function to be printed
  *Return: 1 for lowercase character or 0 for anything else
  */
 int _islower(int c)
 {
 
+
+
 	if (c >= 97 && c <= 122)
 	{
+
 	return (1);
 	}
+
 	return (0);
 }
