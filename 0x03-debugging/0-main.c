749931b2feb9b9cc389a3331682e176a6272e3aa
Dennis Meme <dennismeme46@gmail.com>
2023-03-23 07:44:23 -0700

    0main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main - tests fuction that prints if integer is positive or negagative
- * Return: 0
+ *main - tests fuction that prints if integer is positive or negagative
+ *Return: 0
  */
-
 int main(void)
 
 {
@@ -12,6 +11,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
