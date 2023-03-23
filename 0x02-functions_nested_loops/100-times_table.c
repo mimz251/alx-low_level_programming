004291f03f9ff29dbdd852b921f9c4b183faa6ec
Dennis Meme <dennismeme46@gmail.com>
2023-03-22 19:58:19 -0700

    nhfg

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_times  - a function that prints the n times table.
+ * print_times_table  - a function that prints the n times table.
  * starting with 0.
  * @n: the value of times table printed
  */
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of all multiples of 3 or 5 to 1024
+ *Return: Always (success)
+ */
+int main(void)
+{
+	int i, z = 0;
+
+	while (i < 1024)
+	{
+	z += i;
+	}
+	i++
+	}
+	printf("%d\n", z);
+	return (0);
+}
