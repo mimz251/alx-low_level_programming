721f9d30b73b710d7e62b813b9a370ddca8a97ce
Dennis Meme <dennismeme46@gmail.com>
2023-03-24 00:00:10 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,18 +5,20 @@
  * Return 10 times of the numbers since 0 up to 14
  * /
 
-void more_numbers(void)
-{
-	int x, y;
+ void more_numbers(void)
+ {
+ int x, y;
+ for (x = 0; x < 10; x++)
+ {
+ for (y = 0; y <= 14; y++)
+ {
+ if (y > 9)
+ {
+ _putchar((y / 10) + '0');
+ }
+ _putchar((y % 10) + '0');
+ }
+ _putchar('\n');
+ }
+ }
 
-	for (x = 0; x < 10; x++)
-	{
-	for (y = 0; y <= 14; y++)
-	{
-	_putchar((y / 10) + '0');
-	}
-	_putchar((y % 10) + '0');
-	}
-	_putchar('\n');
-	}
-}
