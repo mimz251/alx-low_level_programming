db928696b25c9be6f57d058b2f387e11dc7f41e9
Dennis Meme <dennismeme46@gmail.com>
2023-03-24 07:26:59 -0700

    debug eve

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -7,21 +7,22 @@
 /**
  * positive_or_negative - Prints if number is positive,zero or negative.
  * Return: void
- *
+ * Description: the numb
+ * @n: variable n
  */
 void positive_or_negative(int n)
 {
 
-        if (n > 0)
-        {
-                printf("%d is positive\n", n);
-        }
-        else if (n == 0)
-        {
-                printf("%d is zero\n", n);
-        }
-        else
-        {
-                printf("%d is negative\n", n);
-        }
-}                             
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+}
