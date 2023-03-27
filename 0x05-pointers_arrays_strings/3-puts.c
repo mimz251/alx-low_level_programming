c2115ccf1f5160b678b392a57bfc71b2ed7975d0
Dennis Meme <dennismeme46@gmail.com>
2023-03-27 12:40:29 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - a function that prints a string
+ *@str: outputs length of string
+ *Return: void
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
