1791f6cc5aa290545eb2b9bb9ff91775e41bf6a0
Dennis Meme <dennismeme46@gmail.com>
2023-03-27 12:59:36 -0700

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_rev - prints a string in rev
+ *@s: prints in rev
+ *Return: 0
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
