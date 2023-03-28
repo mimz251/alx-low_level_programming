20507f0686e5c4409ce5eae9f3609acb52c2c720
Dennis Meme <dennismeme46@gmail.com>
2023-03-28 05:07:32 -0700

    code that prints every other char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *puts2 - a function that prints every other character of a string
+ *@str: character of a string
+ *
+ */
+void puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+		_putchar('\n');
+}
