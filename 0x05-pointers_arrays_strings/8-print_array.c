8959d3240479ed7de73a253d9643152d518cab85
Dennis Meme <dennismeme46@gmail.com>
2023-03-28 07:02:28 -0700

    code that prints arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_array - function that prints n elements of an array of integers
+ *@n:number to be printed
+ *@a:array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+	}
+	if (i !=  (n - 1))
+
+	{
+	printf(", ");
+	}
+	{
+	printf("\n");
+	}
+}
