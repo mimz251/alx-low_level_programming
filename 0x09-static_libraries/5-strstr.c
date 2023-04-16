4232a82015f29d1e0277d87a9e994f180c29e53c
Dennis Meme <dennismeme46@gmail.com>
2023-04-16 08:09:46 -0700

    alx-low_level_programming

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,6 @@
+#include <ctype.h>
+#include <string.h>
+char *_strstr(char *haystack, char *needle)
+{
+	return strstr(haystack, needle);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+int _abs(int n)
+{
+	return abs(n);
+}
