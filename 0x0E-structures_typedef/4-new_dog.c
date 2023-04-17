b5b20f90acc5b007f039fdf4c15f13b011341221
Dennis Meme <dennismeme46@gmail.com>
2023-04-17 10:06:25 -0700

    Write a function that creates  new dogs.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -43,3 +43,43 @@ char *_strcpy(char *dest, char *src)
 	dest[i] = '\0';
 	return (dest);
 }
+/**
+*new_dog - creates a new dog
+*@name:dogs name
+*@age:dogs age
+*@owner:dogs owner
+*Return: pointer to new dog (Success),else NULL.
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int len1, len2;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+	dog = malloc(sizeof(dog_t));
+
+	if (dog == NULL)
+		return (NULL);
+	
+	dog->name = malloc(sizeof(char) *(len1 + 1));
+
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
+}
