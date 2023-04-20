cba575dabba38f76bbbeb54cf95b49c7236448c1
Dennis Meme <dennismeme46@gmail.com>
2023-04-19 19:31:13 -0700

    a program that prints the opcodes of its own main function.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,7 @@
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
-	char *op;
+	char *operator;
 
 	if (argc != 4)
 	{
@@ -20,10 +20,10 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
-	op = argv[2];
+	operator = argv[2];
 	num2 = atoi(argv[3]);
 
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	if (get_op_func(op) == NULL || operator[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
